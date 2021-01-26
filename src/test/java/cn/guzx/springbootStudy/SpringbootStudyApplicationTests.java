be95c7eafc180f39b63a34930c15890de2e29c91
hnguzx <hnguzx@qq.com>
2021-01-26 21:35:35 +0800

    通过spring init自动生成的项目

---
+++ src/test/java/cn/guzx/springbootStudy/SpringbootStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.guzx.springbootStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

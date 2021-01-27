2ec7263654dfcd8db0841a8350b5bb819607b31d
hnguzx <hnguzx@qq.com>
2021-01-27 23:18:00 +0800

    日志

--- src/test/java/cn/guzx/springbootStudy/SpringbootStudyApplicationTests.java
+++ src/test/java/cn/guzx/springbootStudy/SpringbootStudyApplicationTests.java
@@ -2,6 +2,8 @@ package cn.guzx.springbootStudy;
 
 import cn.guzx.bean.Person;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +20,13 @@ class SpringbootStudyApplicationTests {
 	@Value("${chineseTest}")
 	String chineseTest;
 
+	Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Test
 	void testPerson(){
-		System.out.println(person);
+//		System.out.println(person);
+		logger.trace("trance");
+		logger.warn("logging");
 	}
 
 	@Test
